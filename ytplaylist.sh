8b6c2dcfda4781fec8fb55dcf0685bfcc4087ec8
dev <dev@devs-Laptop.local>
2022-12-07 21:54:00 +0800

    updated playlist download function

--- ytplaylist.sh
+++ ytplaylist.sh
@@ -1,4 +1,4 @@
-!/usr/bin/zsh
+#!/usr/bin/zsh
 
-cd /Users/dev/Desktop/python
+cd /Users/dev/Desktop/python # change directory first to where your py file dir before run this
 python3 ydplaylist.py $1
