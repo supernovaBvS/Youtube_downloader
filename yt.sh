b69a9d8f193e9cace73e2c45bb9592993f15acdd
dev <dev@devs-Laptop.local>
2022-12-07 21:55:19 +0800

    youtube downloader sh

--- yt.sh
+++ yt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
-cd /Users/dev/Desktop/python  # change directory first to where your py file dir before run this
+cd /Users/dev/Desktop/python  # change directory first to where your py file dir before run  this
 python3 yd.py $1
